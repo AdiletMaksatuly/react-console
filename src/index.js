120612e35a3143eb2c21dc171fbeb86e0e422c4d
Adilet <bakeryamaker1@gmail.com>
2022-04-15 16:26:30 +0600

    deploy

--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const Global = createGlobalStyle`
+  body {
+    margin: 0;
+  }
+  * {
+    box-sizing: border-box;
+    font-family: consolas;
+  }
+`;
+
+const theme = {
+    colors: {
+        primary: 'green',
+        secondary: '#fff',
+    },
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <ThemeProvider theme={theme}>
+        <Global />
+        <App />
+    </ThemeProvider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
